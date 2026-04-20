2d66c2d777daaec72a3af84f44f2ec6aa7c5764a
Saivanshika10 <saivanshika11@gmail.com>
2026-04-21 00:03:51 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,16 @@
+const BASE_URL = "http://localhost:3000/api";
+
 export async function getStats() {
-  const res = await fetch("http://localhost:3000/api/stats");
+  const res = await fetch(`${BASE_URL}/stats`);
+  return res.json();
+}
+
+export async function getAccounts() {
+  const res = await fetch(`${BASE_URL}/accounts`);
+  return res.json();
+}
+
+export async function getTransactions() {
+  const res = await fetch(`${BASE_URL}/transactions`);
   return res.json();
 }
